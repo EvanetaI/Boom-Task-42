fb401c1d99b5853314bc86d4e61fdd9b07c3f0ef
Evaneta <eftimovaevaneta@gmail.com>
2023-02-12 19:35:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import {AnimatePresence, motion} from "framer-motion/dist/framer-motion"
 
 function App() {
+
   return (
     <div className="App">
-      <form>
+      <motion.form 
+      initial={{x : "0"}}
+      animate={{ position: "fixed" ,top:"50%", left:" 50%", transform: "translate(-50%, -50%)"  }} >
         <h1>Create An Account</h1>
         <label>
           Email:
@@ -14,7 +18,7 @@ function App() {
           <input name="password" type="password" required />
         </label>
         <button>Sign Up!</button>
-      </form>
+      </motion.form>
     </div>
   );
 }
